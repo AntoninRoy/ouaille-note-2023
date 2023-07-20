a2e1ea39e4c932a65e85e67d967018c8c087b8bb
Antonin Roy <anto.roy79@gmail.com>
2023-07-20 16:52:24 +0200

    fix: bug taille iframe

--- pages/benevole.tsx
+++ pages/benevole.tsx
@@ -1,4 +1,4 @@
-import { createRef } from "react";
+import { createRef, useEffect, useRef, useState } from "react";
 import Layout from "../components/Layout/Layout";
 
 export default function Benevole() {
@@ -7,24 +7,27 @@ export default function Benevole() {
   const pageName = "";
 
 
-
   return (
     <>
       <Layout description={description} title={title} pageName={pageName}>
         <div className="mentions">
           <h1 className="heading1">Inscription Bénévole Ouaille Note #9</h1>
-          <iframe
-            onLoad={(event) => {
-                console.log("EHOH")
-            }}
-            src="https://docs.google.com/forms/d/e/1FAIpQLSdkDTcQvlhX2PdjCsISBWz000X5icKJGxGnk17dLy6-8NilsQ/viewform?embedded=true"
-            style={{
-              width: "95vw",
-            }}
-            height="10000"
-          >
-            Chargement…
-          </iframe>
+          <div>
+            <iframe
+              onLoad={(event) => {
+                console.log("EHOH");
+              }}
+              src="https://docs.google.com/forms/d/e/1FAIpQLSdkDTcQvlhX2PdjCsISBWz000X5icKJGxGnk17dLy6-8NilsQ/viewform?embedded=true"
+              style={{
+                width: "90vw",
+                height: "calc(100vh - 200px)",
+                margin: "auto",
+
+              }}
+            >
+              Chargement…
+            </iframe>
+          </div>
         </div>
       </Layout>
     </>
